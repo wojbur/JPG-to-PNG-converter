d484c4667600eff3967104f12930a87d345ed7f2
Wojciech Burman <wojbur94@gmail.com>
2022-10-02 15:35:46 +0200

    add select files function

--- image_converter.py
+++ image_converter.py
@@ -5,18 +5,29 @@ import os
 def select_files() -> tuple:
     root = tk.Tk()
     root.withdraw()
-    return fd.askopenfilenames(parent=root,title='Select images', filetypes=[('Images', '.jpg .jpeg .jpe .png .bmp .tif .tiff')])
+    files = fd.askopenfilenames(parent=root,title='Select images', filetypes=[('Images', '.jpg .jpeg .jpe .png .bmp .tif .tiff')])
+    root.destroy()
+    return files
+
+def select_files_button():
+    pass
 
 # Set up window
 window = tk.Tk()
+window.geometry("700x350")
 window.title('Image Converter')
 
+C = []
 # Set up buttons
-frm_input = tk.Frame(window, relief=tk.GROOVE, bd=1)
-btn_select_files = tk.Button(frm_input, text='Select images', command=select_files)
+frame_input = tk.Frame(window, relief=tk.GROOVE, bd=1)
+button_select_files = tk.Button(frame_input, text='Select images', command= lambda: C==select_files())
 
-btn_select_files.grid(row=0, column=0)
+button_select_files.grid(row=0, column=0)
 
-frm_input.grid(row=0, column=0)
+# Set up selected files listbox
+# lstbox_input = tk.Listbox(frm_input, )
+print(C)
+
+frame_input.grid(row=0, column=0)
 
 window.mainloop()
